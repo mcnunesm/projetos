4ef695640cd8ec0a7ebdf0c9605bfe4100859a46
mcnunesm <marianunesm@hotmail.com>
2023-03-20 09:58:53 -0300

    validação atualizada

--- js/validacao.js
+++ js/validacao.js
@@ -1,90 +1,56 @@
-// Validação Nome
+function validarInput(input, erro, mensagem, validacao) {
+    input.addEventListener('invalid', function (evento) {
+        evento.preventDefault();
+        if (input.validity.valueMissing) {
+            erro.textContent = 'O campo não pode ser deixado em branco';
+        } else if (input.validity.typeMismatch) {
+            erro.textContent = mensagem;
+        }
+        erro.style.display = 'block';
+    });
+
+    input.addEventListener('input', function (evento) {
+        if (validacao(evento.target.value)) {
+            erro.style.display = 'none';
+        } else {
+            erro.textContent = mensagem;
+            erro.style.display = 'block';
+        }
+    });
+}
 
 const nomeInput = document.getElementById('nome');
 const nomeErro = document.querySelector('.contato__nome .input-mensagem-erro');
 
-
-nomeInput.addEventListener('invalid', function (evento) {
-    evento.preventDefault();
-    if (nomeInput.validity.valueMissing) {
-        nomeErro.textContent = 'O campo de nome não pode ser deixado em branco';
-    }
-    nomeErro.style.display = 'block';
+validarInput(nomeInput, nomeErro, 'O nome deve ter no máximo 50 caracteres', function (valor) {
+    return valor.length <= 50;
 });
-
-nomeInput.addEventListener('input', function (evento) {
-    const nome = evento.target.value;
-    if (nome.length > 50) {
-        nomeErro.textContent = 'O nome deve ter no máximo 50 caracteres';
-        nomeErro.style.display = 'block';
-    } else {
-        nomeErro.style.display = 'none';
-    }
-});
-
-// Validação e-mail
 
 const emailInput = document.getElementById('email');
 const emailErro = document.querySelector('.contato__email .input-mensagem-erro');
 
-emailInput.addEventListener('invalid', function (evento) {
-    evento.preventDefault();
-    if (emailInput.validity.valueMissing) {
-        emailErro.textContent = 'O campo de e-mail não pode ser deixado em branco';
-    } else if (emailInput.validity.typeMismatch) {
-        emailErro.textContent = 'Por favor, insira um endereço de e-mail válido';
-    }
-    emailErro.style.display = 'block';
+validarInput(emailInput, emailErro, 'Por favor, insira um endereço de e-mail válido', function (valor) {
+    return valor.match(/^[^@\s]+@[^@\s]+\.[^@\s]+$/);
 });
-
-emailInput.addEventListener('input', function (evento) {
-    if (emailInput.validity.valid) {
-        emailErro.style.display = 'none';
-    }
-});
-
-//Validação assunto
 
 const assuntoInput = document.getElementById('assunto')
 const assuntoErro = document.querySelector('.contato__assunto .input-mensagem-erro')
 
-assuntoInput.addEventListener('invalid', function (evento) {
-    evento.preventDefault()
-    if (assuntoInput.validity.valueMissing) {
-        assuntoErro.textContent = 'O campo de assunto não pode ser deixado em branco'
-        assuntoErro.style.display = 'block'
-    }
-})
-
-assuntoInput.addEventListener('input', function (evento) {
-    if (assuntoInput.validity.valid) {
-        assuntoErro.style.display = 'none';
-    }
+validarInput(assuntoInput, assuntoErro, 'O campo assunto deve ter no máximo 50 caracteres', function (valor) {
+    return valor.length <= 50;
 });
-
-assuntoInput.addEventListener('input', function (evento) {
-    const assunto = evento.target.value
-    if (assunto.length > 50) {
-        assuntoErro.textContent = 'O campo assunto deve ter no máximo 50 caracteres'
-        assuntoErro.style.display = 'block'
-    } else {
-        assuntoErro.style.display = 'none';
-    }
-})
-
-//Validação mensagem
 
 const mensagemInput = document.getElementById('mensagem');
 const mensagemErro = mensagemInput.nextElementSibling;
 
-mensagemInput.addEventListener('blur', function() {
-  if (mensagemInput.value.trim() === '') {
-    mensagemErro.textContent = 'O campo de mensagem não pode ser deixado em branco.';
-    mensagemErro.style.display = 'block';
-  } else if (mensagemInput.value.length > 300) {
-    mensagemErro.textContent = 'O campo de mensagem não pode ter mais de 300 caracteres.';
-    mensagemErro.style.display = 'block';
-  } else {
-    mensagemErro.style.display = 'none';
-  }
+mensagemInput.addEventListener('blur', function () {
+    if (mensagemInput.value.trim() === '') {
+        mensagemErro.textContent = 'O campo de mensagem não pode ser deixado em branco.';
+        mensagemErro.style.display = 'block';
+    } else if (mensagemInput.value.length > 300) {
+        mensagemErro.textContent = 'O campo de mensagem não pode ter mais de 300 caracteres.';
+        mensagemErro.style.display = 'block';
+    } else {
+        mensagemErro.style.display = 'none';
+    }
 });
